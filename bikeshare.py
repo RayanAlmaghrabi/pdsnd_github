314e8cc6f9c6f7f3e3c1fcbe1b2a14f74395e040
RayanAlmaghrabi <ryan7530@gmail.com>
2023-02-20 08:52:21 -0500

    added a print statement at the end

--- bikeshare.py
+++ bikeshare.py
@@ -215,6 +215,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print('program closing')
             break
 
 
